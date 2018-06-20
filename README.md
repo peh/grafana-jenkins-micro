e8df659fbc5c02fdeb3cbf4f702249787f8ef2cb
Philipp Eschenbach <pesch3@gmail.com>
2018-06-20 14:33:09 +0200

    fix the test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # grafana-jenkins-micro
+A Microservice giving you information about remaining build time from Jenkins in Grafana.
+
+Requires [the simple-json-datasource plugin](https://github.com/grafana/simple-json-datasource) to be installed in grafana
+ 
--- src/test/groovy/com/uberall/GrafanaControllerSpec.groovy
+++ src/test/groovy/com/uberall/GrafanaControllerSpec.groovy
@@ -1,23 +1,27 @@
-package com.uberall;
+package com.uberall
 
 import io.micronaut.context.ApplicationContext
-import io.micronaut.runtime.server.EmbeddedServer
-import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.runtime.server.EmbeddedServer
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
 class GrafanaControllerSpec extends Specification {
 
-    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
-    @Shared @AutoCleanup RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+    @Shared
+    @AutoCleanup
+    RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
 
 
     void "test index"() {
         given:
-        HttpResponse response = client.toBlocking().exchange("/grafana")
+        HttpResponse response = client.toBlocking().exchange("/")
 
         expect:
         response.status == HttpStatus.OK
